ed0998b3f63ff4ca3026e46138fc5956397d31f8
AvaniAbhijit <avani.abhijit@orchids.edu.in>
2024-06-24 15:04:04 +0530

    Add files via upload

---
+++ Maze Game/Solutions(TRM)/G7 1.1 A Creating screen.py
@@ -0,0 +1,5 @@
+import pygame #import pygame module
+pygame.init() #to intiialize all the imported pygame modules
+
+# Set up the drawing window using width and height
+window = pygame.display.set_mode([500, 500]) #screen just flashes
